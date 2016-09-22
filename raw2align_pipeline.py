f76ee60abfd6d5532955d2cb6f5b227f872eb567
shendri4 <shendri4@gmail.com>
2016-09-22 12:24:29 -0700

    removed GATK commands

--- raw2align_pipeline.py
+++ raw2align_pipeline.py
@@ -27,16 +27,15 @@ for l in open('samples.txt'):
     if len(l) > 1:
         samples.append(l.split('/')[-1].replace('_R1_001.fastq.gz', '').strip())
 
-#print samples
 
 # Setup folders and paths variables:
 resultsDir = '01-Cleaned'
 bamFolder = '02-Mapped'
 variantFolder = '03-Calls'
-gatkPath = '/opt/modules/biology/gatk/3.5/bin/GenomeAnalysisTK.jar'
+#gatkPath = '/opt/modules/biology/gatk/3.5/bin/GenomeAnalysisTK.jar'
 rawdataDir = '/mnt/lfs2/hend6746/fox_cancer/0rawdata_test'
 bwaIndex = '/mnt/lfs2/hend6746/wolves/reference/canfam31/canfam31.fa'
-gatkCall = 'java -jar /opt/modules/biology/gatk/3.5/bin/GenomeAnalysisTK.jar -R %s -T HaplotypeCaller' % bwaIndex
+#gatkCall = 'java -jar /opt/modules/biology/gatk/3.5/bin/GenomeAnalysisTK.jar -R %s -T HaplotypeCaller' % bwaIndex
 os.system('mkdir -p %s' % resultsDir)
 os.system('mkdir -p %s' % bamFolder)
 os.system('mkdir -p %s' % variantFolder)
@@ -132,29 +131,11 @@ for sample in samples:
     log(cmd, logCommands)
     os.system(cmd)
 
-    #gatkCall += ' -I ' + jp(bamFolder, sample) + ".bam"
-    #logCommands.close()
-
-    #Call SNPs with GATK
-    logFile = jp(variantFolder, sample + '_GATK.log')
-    cmd = ' '.join([gatkCall,  ' -I ' + jp(bamFolder, sample) + ".bam", 
-                    ' -o ' + jp(variantFolder, sample) + ".raw.variants.vcf", '>>', logFile, '2>&1'])
-    log(cmd, logCommands)
+    # Clean up sam files:
+    cmd = ' '.join(['rm', jp(bamFolder, "*.sam")])
     os.system(cmd)
+    logCommands.close()
+
     
 '''
-gatkCall += ' -o output.raw.variants.vcf'
 
-print "Now call gatk with:\n"+gatkCall
-with open("run_gatk.sh", 'w') as outf:
-    outf.write(gatkCall + '\n')
-    
-# Clean up sam files:
-cmd = ' '.join(['rm', jp(bamFolder, "*.sam")])
-os.system(cmd)
-
-##### Merge all bam files
-#cmd = ' '.join(['samtools merge', jp(bamFolder, "*.bam")])
-
-#java -jar /opt/modules/biology/gatk/3.5/bin/GenomeAnalysisTK.jar -R /dev/shm/bwaidx/GRCH38_bwa.fa -T HaplotypeCaller -I test.bam -o output.raw.snps.indels.vcf
-'''
