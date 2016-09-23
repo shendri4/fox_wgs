b1fcc52bc85497c199ac29b9030ae68e9c03fe15
shendri4 <shendri4@gmail.com>
2016-09-23 16:02:20 -0700

    changed path and fixed sample name for JAR samples

--- raw2align_pipeline.py
+++ raw2align_pipeline.py
@@ -26,7 +26,7 @@ def log(txt, out):
 samples = []
 for l in open('samples.txt'):
     if len(l) > 1:
-        samples.append(l.split('/')[-1].replace('_R1_001.fastq.gz', '').strip())
+        samples.append(l.split('/')[-1].replace('_R1.fq.gz', '').strip())
 
 
 # Setup folders and paths variables:
@@ -34,7 +34,7 @@ resultsDir = '01-Cleaned'
 bamFolder = '02-Mapped'
 variantFolder = '03-Calls'
 #gatkPath = '/opt/modules/biology/gatk/3.5/bin/GenomeAnalysisTK.jar'
-rawdataDir = '/mnt/lfs2/hend6746/fox_cancer/0rawdata'
+rawdataDir = '/mnt/lfs2/hend6746/fox_cancer/fox_JAR'
 bwaIndex = '/mnt/lfs2/hend6746/wolves/reference/canfam31/canfam31.fa'
 #gatkCall = 'java -jar /opt/modules/biology/gatk/3.5/bin/GenomeAnalysisTK.jar -R %s -T HaplotypeCaller' % bwaIndex
 os.system('mkdir -p %s' % resultsDir)
