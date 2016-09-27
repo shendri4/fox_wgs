b7da7a7dd75eb6506099c5eb60918868f9461671
shendri4 <shendri4@gmail.com>
2016-09-27 10:38:13 -0700

    created script to run fastqc

---
+++ fastqc.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python	
+###	Usage ./fastqc.py
+
+### Make sure to load:
+### module load fastqc	
+
+from os.path import join as jp
+import os
+import sys
+
+VERBOSE=False
+
+#Function definitions:
+def log(txt, out):
+    if VERBOSE:
+        print(txt)
+    out.write(txt+'\n')
+    out.flush()
+
+## Read in samples and put them in a list:
+samples = []
+for l in open('samples.txt'):
+    if len(l) > 1:
+        samples.append(l.split('/')[-1].replace('_R1.fq.gz', '').strip())
+
+# Setup folders and paths variables:
+rawdataDir = '/mnt/lfs2/hend6746/fox_cancer/01rawdata_test'
+cleandataDir = '01-Cleaned'
+bamFolder = '02-Mapped'
+resultsDir = '/mnt/lfs2/hend6746/fox_cancer/fastqc_test'
+os.system('mkdir -p %s' % resultsDir)
+
+
+##### Run pipeline ###
+for sample in samples:
+    #raw
+    cmd = ' '.join(['fastqc', '--outdir', resultsDir, '--format', fastq, jp(rawdataDir, sample + '_R1_001.fastq.gz')]) 
+    cmd = ' '.join(['fastqc', '--outdir', resultsDir, '--format', fastq, jp(rawdataDir, sample + '_R2_001.fastq.gz')])
+'''    
+    #cleaned
+    cmd = ' '.join(['fastqc', '--outdir', resultsDir, '--format', fastq, jp(cleandataDir, sample + '_cleaned_PE1.fastq.gz')]) 
+    cmd = ' '.join(['fastqc', '--outdir', resultsDir, '--format', fastq, jp(cleandataDir, sample + '_cleaned_PE2.fastq.gz')])
+    cmd = ' '.join(['fastqc', '--outdir', resultsDir, '--format', fastq, jp(cleandataDir, sample + '_cleaned_SE.fastq.gz')]) 
+
+    #bam
+    cmd = ' '.join(['fastqc', '--outdir', resultsDir, '--format', bam, jp(bamFolder, sample + '.bam')]) 
+'''
