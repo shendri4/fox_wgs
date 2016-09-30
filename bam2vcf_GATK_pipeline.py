f559c26daa06c9a2c14858057bf0ca8f65b1f1d7
shendri4 <shendri4@gmail.com>
2016-09-29 21:12:55 -0700

    modified script to haplotype caller

--- bam2vcf_GATK_pipeline.py
+++ bam2vcf_GATK_pipeline.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 #import argparse
 #from glob import glob
-'''
-Make sure to load:
-module load python/2.7.10
-module load bwa
-module load grc
-module load samtools
-'''
 
 from os.path import join as jp
+from os.path import abspath
 import os
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', "--samples", help="Samples.txt file with sample ID.", required=True)
+parser.add_argument('-r', "--rawdata", help="Path to raw fastq data.", required=True)
+parser.add_argument('-b', "--bwaindex", help="Path to bwa index file.", required=True)
+args = parser.parse_args()
 
 VERBOSE=False
 
@@ -23,45 +25,105 @@ def log(txt, out):
 
 ## Read in samples and put them in a list:
 samples = []
-for l in open('samples.txt'):
+for l in open(args.samples):
     if len(l) > 1:
         samples.append(l.split('/')[-1].replace('_R1_001.fastq.gz', '').strip())
 
-#print samples
 
 # Setup folders and paths variables:
-resultsDir = '01-Cleaned'
-bamFolder = '02-Mapped'
-variantFolder = '03-Calls'
-gatkPath = '/opt/modules/biology/gatk/3.5/bin/GenomeAnalysisTK.jar'
-rawdataDir = '/mnt/lfs2/hend6746/fox_cancer/0rawdata_test'
-bwaIndex = '/mnt/lfs2/hend6746/wolves/reference/canfam31/canfam31.fa'
-gatkCall = 'java -jar /opt/modules/biology/gatk/3.5/bin/GenomeAnalysisTK.jar -R %s -T HaplotypeCaller' % bwaIndex
+bamFolder = abspath('02-Mapped')
+variantFolder = abspath('03-Calls')
+PBS_scripts = abspath('PBS_scripts')
+rawdataDir = abspath(args.rawdata)
+bwaIndex = abspath(args.bwaindex)
+gatkCall = 'java -jar /opt/modules/biology/gatk/3.5/bin/GenomeAnalysisTK.jar -R %s' % bwaIndex
+
+
 os.system('mkdir -p %s' % resultsDir)
 os.system('mkdir -p %s' % bamFolder)
 os.system('mkdir -p %s' % variantFolder)
+os.system('mkdir -p %s' % PBS_scripts)
 
 ##### Run pipeline ###
 for sample in samples:
     print "Processing", sample, "....."
     # Set up files:
-    logCommands = open(jp(resultsDir, sample + '_commands.log'), 'w')
-
-    #Call SNPs with GATK
     logFile = jp(variantFolder, sample + '_GATK.log')
+    logCommands = open(jp(PBS_scripts, sample + '_commands.sh'), 'w')
+
+    #Setup for qsub
+    log('#!/bin/bash', logCommands)
+    log('#PBS -N %s' % sample, logCommands)
+    log('#PBS -j oe', logCommands)
+    log('#PBS -o %s_job.log' % sample, logCommands)
+    log('#PBS -m abe', logCommands)
+    log('#PBS -M shendri4@gmail.com', logCommands)
+    log('#PBS -q short', logCommands)
+    log('#PBS -l mem=100000', logCommands)
+    log(". /usr/modules/init/bash", logCommands)
+    log("module load python/2.7.10", logCommands)
+    log("module load grc", logCommands)
+
+##### Run pipeline ###
+for sample in samples:
+    print "Processing", sample, "....."
+
+'''
+    #indel realignment is no longer necessary for variant discovery if you plan to use a variant caller 
+    #that performs a haplotype assembly step, such as HaplotypeCaller
+    #RealignerTargetCreator
+    cmd = ' '.join([gatkCall,  '-T RealignerTargetCreator', ' -I ' + jp(bamFolder, sample) + ".bam", 
+                    ' -o ' + jp(bamFolder, sample) + ".RealignerTargetCreator.intervals", '>>', logFile, '2>&1'])
+    log(cmd, logCommands)
+    #os.system(cmd)
+    
+    #Realignment Around Indels
+    cmd = ' '.join([gatkCall, '-T IndelRealigner', ' -I ' + jp(bamFolder, sample) + ".bam", 
+                    ' -o ' + jp(bamFolder, sample) + ".realign.bam", 
+                    ' -targetIntervals '  + jp(bamFolder, sample) + ".RealignerTargetCreator.intervals" 
+                    '>>', logFile, '2>&1'])
+    log(cmd, logCommands)
+    #os.system(cmd)
+
+    #Base Quality Score Recalibration
     cmd = ' '.join([gatkCall,  ' -I ' + jp(bamFolder, sample) + ".bam", 
                     ' -o ' + jp(variantFolder, sample) + ".raw.variants.vcf", '>>', logFile, '2>&1'])
     log(cmd, logCommands)
-    os.system(cmd)
+    #os.system(cmd)
+'''
+    ###########Per-Sample Variant Calling
+    #HaplotypeCaller on each sample BAM file 
+    #(if a sample's data is spread over more than one BAM, then pass them all in together) to create single-sample gVCFs
+    #not recommended for somatic (cancer) variant discovery. For that purpose, use MuTect2 instead
+    cmd = ' '.join([gatkCall,  ' -T HaplotypeCaller ', ' -I ' + jp(bamFolder, sample) + ".bam", 
+                    ' --emitRefConfidence GVCF ', ' -o ' + jp(variantFolder, sample) + ".raw.snps.indels.g.vcf", '>>', logFile, '2>&1'])
+    log(cmd, logCommands)
+    #os.system(cmd)
     
 '''
-gatkCall += ' -I ' + jp(bamFolder, sample) + ".bam"
-logCommands.close()
-gatkCall += ' -o output.raw.variants.vcf'
+for i in samples; do variant=--variant $i;    
+    ###########Joint Genotyping
+    cmd = ' '.join(['for i in samples; do variant=--variant $i;', gatkCall,  ' -T GenotypeGVCFs ', ' ${variant} ' + jp(variantFolder, sample) + ".raw.snps.indels.g.vcf", 
+                    ' -o ' + jp(variantFolder, sample) + "raw.variants.vcf", '>>', logFile, '2>&1'])
+    log(cmd, logCommands)
+    #os.system(cmd)
 
-print "Now call gatk with:\n"+gatkCall
-with open("run_gatk.sh", 'w') as outf:
-    outf.write(gatkCall + '\n')
+    ############Variant Quality Score Recalibration
+    #SNPs
+    cmd = ' '.join([gatkCall,  ' -T VariantRecalibrator ', ' -I ' + jp(variantFolder, sample) + "raw.variants.vcf",
+                    ' -resource:????????????????? '
+                    ' -an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR -an InbreedingCoeff ', 
+                    ' -mode SNP ', ' -recalFile output.recal ', ' -tranchesFile output.tranches ', ' -rscriptFile output.plots.R ', '>>', logFile, '2>&1'])
+    log(cmd, logCommands)
+    #os.system(cmd)
+    
+    #Indels
+    cmd = ' '.join([gatkCall,  ' -T VariantRecalibrator ', ' -I ' + jp(variantFolder, sample) + "raw.variants.vcf",
+                	' -resource:????????????????? '
+                    ' -an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR -an InbreedingCoeff ', 
+                    ' -mode INDEL ', ' -recalFile output.recal ', ' -tranchesFile output.tranches ', ' -rscriptFile output.plots.R ', '>>', logFile, '2>&1'])
+    log(cmd, logCommands)
+    #os.system(cmd)
     
 ##### Merge all bam files
 #cmd = ' '.join(['samtools merge', jp(bamFolder, "*.bam")])
