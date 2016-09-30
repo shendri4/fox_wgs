056ee5e61819c506fbb917b1fd581ad6673d6d68
shendri4 <shendri4@gmail.com>
2016-09-30 15:30:17 -0700

    trying to fix bug

--- jointgenotyping_GATK.py
+++ jointgenotyping_GATK.py
@@ -62,11 +62,11 @@ log("module load grc", logCommands)
 variants = []
 for sample in samples:
     sample = ' '.join(['--variant ' + jp(variantFolder, sample) + '.raw.snps.indels.g.vcf'])
-#    print sample
     variants.append(sample)
         #variants.append(l.join(['--variant ' + jp(variantFolder, sample) + '.raw.snps.indels.g.vcf'].strip('/n').split('\t'))
 print variants
-
+variantList = ''.join(variants)
+print variantList
 ###########Joint Genotyping
 cmd = ' '.join([gatkCall, ' -T GenotypeGVCFs ', variants, ' -o ' + jp(variantFolder) + 'joint.variants.vcf', '>>', logFile, '2>&1'])
 log(cmd, logCommands)
