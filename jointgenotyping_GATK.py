bad04bdac3d32af4e9fd31a228ff10aab4e30d43
shendri4 <shendri4@gmail.com>
2016-09-30 14:44:41 -0700

    trying to fix bug

--- jointgenotyping_GATK.py
+++ jointgenotyping_GATK.py
@@ -62,7 +62,7 @@ log("module load grc", logCommands)
 for sample in samples:
     print "Processing", sample, "....."
     variants = []
-#     for l in samples:
+    for l in samples:
 #         if len(l) > 1:
 #             variants.append(l.join(['--variant ' + jp(variantFolder, sample) + '.raw.snps.indels.g.vcf'].strip('/n').split('\t'))
 
